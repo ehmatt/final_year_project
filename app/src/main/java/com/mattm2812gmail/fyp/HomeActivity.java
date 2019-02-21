cf3eec76dd9b8908b6013aac3d2d8873f432b0c1
ehmatt <mattm2812@gmail.com>
2019-02-21 12:26:50 +0000

    refactor login/home pages

---
+++ app/src/main/java/com/mattm2812gmail/fyp/HomeActivity.java
@@ -0,0 +1,20 @@
+package com.mattm2812gmail.fyp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity);
+    }
+
+    public void addTask(View view){
+        // open menu then add task
+    }
+}
--- app/src/main/java/com/mattm2812gmail/fyp/MainActivity.java
+++ app/src/main/java/com/mattm2812gmail/fyp/MainActivity.java
@@ -2,23 +2,49 @@ package com.mattm2812gmail.fyp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.content.Intent;
 import android.view.View;
+import android.graphics.Color;
+
+import android.content.Intent;
+
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button login;
+    EditText username, password;
+//    TextView t1;
+    int count = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-//    public void addTask(View view){
-//        Intent intent = new Intent(this, DisplayMessageActivity.class);
-//        EditText editText = (EditText) findViewById(R.id.editText);
-//        String message = editText.getText().toString();
-//        intent.putExtra(EXTRA_MESSAGE, message);
-//        startActivity(intent);
-//    }
+        login = (Button)findViewById(R.id.button);
+        username = (EditText)findViewById(R.id.editText1);
+        password = (EditText)findViewById(R.id.editText2);
+
+        login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if (username.getText().toString().equals("matt") && password.getText().toString().equals("password")){
+                    startActivity(new Intent(MainActivity.this, HomeActivity.class));
+
+                } else {
+                    Toast.makeText(getApplicationContext(), "Wrong username/password", Toast.LENGTH_SHORT).show();
+//                    t1.setVisibility(View.VISIBLE);
+//                    t1.setBackgroundColor(Color.RED);
+
+                    //only 3 login attempts
+                    count--;
+                    if (count==0){
+                        login.setEnabled(false);
+                    }
+                }
+            }
+        });
+    }
 }
